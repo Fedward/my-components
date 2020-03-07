6fe9306a033a1f97e2e80d57fd11f7b85a948b41
Fedor Penkin <piknikfedor@gmail.com>
2020-03-07 12:09:50 +0300

    dot position formula

--- src/components/RangeFilter.vue
+++ src/components/RangeFilter.vue
@@ -3,33 +3,33 @@
     <div>{{lowerVal}}<br>{{upperVal}}</div>
     <div class="range-filter">
       <div class="range">
-        <input type="number" :min="min" :max="upperVal" v-model="lowerVal">
-        <input type="number" :min="lowerVal" :max="max" v-model="upperVal">
+        <input class="range__input" type="number" :min="min" :max="upperVal" v-model="lowerVal">
+        <input class="range__input" type="number" :min="lowerVal" :max="max" v-model="upperVal">
       </div>
 
       <div class="slider" :style="styles.slider">
         <div class="slider__rail" :style="styles.rail"></div>
         <div
           class="slider__inactive-rail"
-          :style="[styles.rail, { width: `${lowerRatio}%` }]"
+          :style="[styles.rail, { width: `${lowerPercentageRatio}%` }]"
         ></div>
         <div
           class="slider__inactive-rail slider__inactive-rail_right"
-          :style="[styles.rail, { width: `${100 - upperRatio}%` }]"
+          :style="[styles.rail, { width: `${100 - upperPercentageRatio}%` }]"
         ></div>
 
         <div class="slider__slide-zone" :style="styles.slideZone">
           <div
             class="slider__dot"
             ref="lower"
-            :style="[styles.dot, { left: `${lowerRatio}%` }]"
-            @mousedown="dragStart($event, 'lower')"
+            :style="[styles.dot, { left: `${lowerPercentageRatio}%` }]"
+            @mousedown="dragStart($event, 'lowe')"
             @touchstart="dragStart($event, 'lower')"
           ></div>
           <div
             class="slider__dot"
             ref="upper"
-            :style="[styles.dot, { left: `${upperRatio}%` }]"
+            :style="[styles.dot, { left: `${upperPercentageRatio}%` }]"
             @mousedown="dragStart($event, 'upper')"
             @touchstart="dragStart($event, 'upper')"
           ></div>
@@ -40,46 +40,57 @@
 </template>
 
 <script>
-const getOffset = (elem) => {
+const getOffsetX = (elem) => {
   const { body, documentElement: doc } = document;
-  const { top, left } = elem.getBoundingClientRect();
-  const offset = {
-    x: left + (window.pageXOffset || doc.scrollLeft) - (doc.clientLeft || body.clientLeft || 0),
-    y: top + (window.pageYOffset || doc.scrollTop) - (doc.clientTop || body.clientTop || 0),
-  };
-  return offset;
+  const { left } = elem.getBoundingClientRect();
+  const { pageXOffset } = window;
+  const offsetX = left + (pageXOffset || doc.scrollLeft) - (doc.clientLeft || body.clientLeft || 0);
+
+  return offsetX;
 };
 
 /**
- * Получить координаты клика относительно
- * верхнего левого угла элемента
+ * Получить координаты по оси X
+ * клика относительно элемента
  *
  * @param {MouseEvent|TouchEvent} e
- * @param {HTMLDivElement} elem - element relative to which coordinates are calculated
+ * @param {HTMLDivElement} elem - element relative to which coordinate X is calculated
  *
- * @returns {object} - coords x,y
+ * @returns {number} - X axis value
  */
-const getCoordsRelativeToElem = (e, elem) => {
-  const event = 'targetTouches' in e ? e.targetTouches[0] : e;
-  const elemOffset = getOffset(elem);
+const getEventOffsetXRelativeToElem = (e, elem) => {
+  const event = e.targetTouches ? e.targetTouches[0] : e;
+  const elemOffsetX = getOffsetX(elem);
+  const eventOffsetXFromElem = event.pageX - elemOffsetX;
 
-  return {
-    x: event.pageX - elemOffset.x,
-    y: event.pageY - elemOffset.y,
-  };
+  return eventOffsetXFromElem;
 };
 
 export default {
   name: 'RangeFilter',
-  props: ['min', 'max', 'dotSize', 'railWidth'],
+  props: {
+    min: {
+      default: 0,
+    },
+    max: {
+      default: 100,
+    },
+    dotSize: {
+      default: 16,
+    },
+    railWidth: {
+      default: 4,
+    },
+    lazy: {
+      default: false,
+    },
+  },
   data() {
     return {
-      lowerVal: Number.parseInt(this.min, 10),
-      upperVal: Number.parseInt(this.max, 10),
-      lowerPos: 0,
-      upperPos: 100,
-      intervalSize: this.max - this.min,
-      haha: '',
+      lowerVal: this.min,
+      upperVal: this.max,
+      gap: 100 / (this.max - this.min),
+      currentDraggableDot: null,
       styles: {
         slider: {
           height: `${this.dotSize}px`,
@@ -99,44 +110,56 @@ export default {
     };
   },
   computed: {
-    lowerRatio() {
-      const ratio = (this.lowerVal - this.intervalSize) / this.intervalSize;
-
-      return ratio * 100;
+    lowerPercentageRatio() {
+      return (this.lowerVal - this.min) * this.gap;
     },
-    upperRatio() {
-      const ratio = (this.upperVal - this.intervalSize) / this.intervalSize;
-
-      return ratio * 100;
+    upperPercentageRatio() {
+      return (this.upperVal - this.min) * this.gap;
     },
-    currentMinPos() {
-      return 0;
+    dotValueRanges() {
+      return {
+        lower: [this.min, this.upperVal],
+        upper: [this.lowerVal, this.max],
+      };
     },
-  },
-  created() {
-    console.log('interval size', this.intervalSize);
-    console.log('lower val', this.lowerVal);
-    console.log('upper val', this.upperVal);
-  },
-  mounted() {
-    // this.bindEvents();
-  },
-  beforeDestroy() {
-    // this.unbindEvents();
   },
   methods: {
     dragStart(e, dotId) {
-      if (e.type === 'touchstart') {
-        console.log('touch');
-      } else {
-        console.log('mouse');
+      this.currentDraggableDot = dotId;
+      e.preventDefault();
+    },
+    dragMove() {
+      if (!this.currentDraggableDot) {
+        return;
       }
 
-      // e.preventDefault();
+    },
+    dragEnd() {
+      this.currentDraggableDot = null;
     },
     getValidPos(pos, dotId) {
 
     },
+    bindEvents() {
+      document.addEventListener('touchmove', this.dragMove);
+      document.addEventListener('touchend', this.dragEnd);
+      document.addEventListener('mousemove', this.dragMove);
+      document.addEventListener('mouseup', this.dragEnd);
+      document.addEventListener('mouseleave', this.dragEnd);
+    },
+    unbindEvents() {
+      document.removeEventListener('touchmove', this.dragMove);
+      document.removeEventListener('touchend', this.dragEnd);
+      document.removeEventListener('mousemove', this.dragMove);
+      document.removeEventListener('mouseup', this.dragEnd);
+      document.removeEventListener('mouseleave', this.dragEnd);
+    },
+  },
+  mounted() {
+    this.bindEvents();
+  },
+  beforeDestroy() {
+    this.unbindEvents();
   },
 };
 </script>
@@ -146,7 +169,6 @@ export default {
   padding: 16px 24px 32px;
   background-color: #F7F7F7;
 }
-
 
 .slider {
   position: relative;
@@ -166,6 +188,7 @@ export default {
     transform: translateY(-50%);
     background-color: #DFDFDF;
     border-radius: 1px;
+    transition: width .15s ease-in-out;
 
     &_right {
       right: 0;
